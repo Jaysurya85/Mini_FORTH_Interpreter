d0c2367bd010ce563caa53d4924bf3f5870113c8
Jaysurya85 <jaysuryaray1998@gmail.com>
2026-02-27 18:15:21 -0500

    Added Basic Calculation

--- Interpret.hs
+++ Interpret.hs
@@ -1,22 +1,25 @@
 module Interpret where
+
 -- this file contains the FORTH interpreter
 
-import Val
 import Eval
 import Flow
+import Val
 
 -- inner function for foldl
--- Takes the current stack and an input and 
+-- Takes the current stack and an input and
 -- computes the next stack
 evalF :: ([Val], String) -> Val -> ([Val], String)
 evalF s (Id op) = evalOut op s
 -- cannot run, put on the stack and preserve output
-evalF (s, out) x = (x:s,out)
+evalF (s, out) x = (x : s, out)
 
--- function to interpret a string into a stack and 
+-- function to interpret a string into a stack and
 -- an output string
 interpret :: String -> ([Val], String)
-interpret text = text |>
-    words |> -- brake text into words
-    map strToVal |> -- strings to instructions
-    foldl evalF ([], "") -- perform evaluation
+interpret text =
+  text
+    |> words
+    |> map strToVal -- brake text into words
+    |> foldl evalF ([], "") -- strings to instructions
+    -- perform evaluation
--- Main.hs
+++ Main.hs
@@ -7,7 +7,14 @@ import System.Environment
 
 main :: IO ()
 main = do
-    (fileName:tl) <- getArgs
-    contents <- readFile fileName
-    let (stack, output) = interpret contents 
-    putStrLn output
+  (fileName : _) <- getArgs
+  contents <- readFile fileName
+  let (stack, output) = interpret contents
+
+  putStrLn output
+
+  if not (null stack)
+    then do
+      putStrLn "Warning: Stack not empty."
+      print stack
+    else return ()
